8ddef49529c443db652e7367fa3b6210075beecf
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-04-11 09:13:45 +0200

    some CSS on Jumbotron and Navbar

--- src/Components/Navigation/NavBar.tsx
+++ src/Components/Navigation/NavBar.tsx
@@ -1,23 +1,52 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { BsFillPeopleFill } from "react-icons/bs";
+import { MdOutlineFace3, MdWorkOutline, MdOutlineMail } from "react-icons/md";
+import { BsBook, BsTools } from "react-icons/bs";
+import { HiOutlineLightBulb } from "react-icons/hi";
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand>
           <div>Logo</div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="me-auto justify-content-end">
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="me-auto">
             <Nav.Link>
-              <div
-                className="d-flex flex-column align-items-center nav-options"
-                //onClick={handleShow2}
-              >
-                <BsFillPeopleFill size="2em" />
-                <span className="nav-text">My Network</span>
+              <div className="icons d-flex flex-column align-items-center mx-2">
+                <MdOutlineFace3 size="1em" />
+                <span className="nav-text">About me</span>
+              </div>
+            </Nav.Link>
+            <Nav.Link>
+              <div className="icons d-flex flex-column align-items-center mx-2">
+                <BsBook size="1em" />
+                <span className="nav-text">Education</span>
+              </div>
+            </Nav.Link>
+            <Nav.Link>
+              <div className="icons d-flex flex-column align-items-center mx-2">
+                <MdWorkOutline size="1em" />
+                <span className="nav-text">Work experience</span>
+              </div>
+            </Nav.Link>
+            <Nav.Link>
+              <div className="icons d-flex flex-column align-items-center mx-2">
+                <BsTools size="1em" />
+                <span className="nav-text">Skills</span>
+              </div>
+            </Nav.Link>
+            <Nav.Link>
+              <div className="icons d-flex flex-column align-items-center mx-2">
+                <HiOutlineLightBulb size="1em" />
+                <span className="nav-text">Project</span>
+              </div>
+            </Nav.Link>
+            <Nav.Link>
+              <div className="icons d-flex flex-column align-items-center mx-2">
+                <MdOutlineMail size="1em" />
+                <span className="nav-text">Contact</span>
               </div>
             </Nav.Link>
           </Nav>
